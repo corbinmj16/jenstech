ec730c1596e81fef8c82ca4d86eb4ef572c59f17
Corbin <corbin.jensen@me.com>
2019-12-18 07:55:29 -0500

    trying to figure out gulp

--- themes/jenstech/gulpfile.js
+++ themes/jenstech/gulpfile.js
@@ -1,5 +1,34 @@
-function defaultTask(cb) {
-  cb();
+const {task, watch, pipe, dest, src, series} = require('gulp');
+const sass = require('gulp-sass');
+
+const SCSS_INPUT = './source/scss/styles.scss';
+const CSS_OUTPUT = './source/css';
+
+function scssWatch() {
+  return watch(SCSS_INPUT, () => {
+    task(css);
+  })
+  .on('change', (event) => {
+    console.log('changed file: ', event);
+  });
 }
 
-exports.default = defaultTask
+function css() {
+  return src(SCSS_INPUT)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest(CSS_OUTPUT));
+}
+
+exports.watchStyles = series(scssWatch);
+exports.default = series(css);
+
+// gulp.task('scss:watch', function() {
+//   return gulp
+//     // Watch input folder for changes
+//     // then run the 'scss' task
+//     .watch(SCSS_INPUT, 'scss')
+//     // Whent here is a change log it to the console
+//     .on('change', (event) => {
+//       console.log(event);
+//     });
+// });
